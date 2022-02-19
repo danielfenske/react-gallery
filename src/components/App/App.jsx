5e4912bccc70b44b9300518dc2f399742f209be9
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-19 13:01:52 -0600

    Imported axios, useEffect, and useState in app.jsx to prepare for axios GET endpoint

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  const [galleryItems, setGalleryItems] = useState([]);
+
+  // =============== USE EFFECT =======================
+  useEffect( () => {
+    console.log('in useEffect');
+    
+    // initiate GET request on page load
+    getGalleryItems();
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg" />
+    </div>
+  );
 }
 
 export default App;
