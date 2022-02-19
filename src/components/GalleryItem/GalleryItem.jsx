7ae3857410f06fffc4d79968d20ea4033fddd7f6
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-19 14:05:05 -0600

    Removed object from galleryItems array; added styling to images and created clickListener for image container in GalleryItem component

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,14 +1,38 @@
+import './GalleryItem.css'
+import { useState } from 'react';
 
-const GalleryItem = ({galleryItem}) => {
+const GalleryItem = ({ galleryItem }) => {
+
+    const [showImage, setShowImage] = useState(true);
+
+    const handleImageContainerClick = () => {
+        console.log('in handleImageClick');
+
+        setShowImage(!showImage);
+    }
+
     return (
         <>
-        <div className="galleryItemContainer">
-            <h1>{galleryItem.title}</h1>
-            <img src={galleryItem.path} className="galleryImage"/>
-            <p>{galleryItem.description}</p>
-            <h1>{galleryItem.location}</h1>
-            <p>Likes: {galleryItem.likes}</p>
-        </div>
+            <div className="galleryItemContainer">
+                <h1>{galleryItem.title}</h1>
+                <div className="imageContainer">
+                    {
+                        showImage ?
+                                <img
+                                src={galleryItem.path}
+                                className="galleryImage"
+                                onClick={handleImageContainerClick}
+                                />
+                            :   <p
+                                onClick={handleImageContainerClick}
+                                >
+                                {galleryItem.description}
+                                </p>
+                    }
+                </div>
+                <h1>{galleryItem.location}</h1>
+                <p>Likes: {galleryItem.likes}</p>
+            </div>
         </>
     )
 }
