e90ced2bb9985a7020250cafe7f97bc14b629a09
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-19 13:08:03 -0600

    Setup axios GET request from client to receive all galleryItems in array held in server; tested to operate correctly

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,18 +2,40 @@ import React from 'react';
 import './App.css';
 
 import axios from 'axios';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [galleryItems, setGalleryItems] = useState([]);
 
   // =============== USE EFFECT =======================
-  useEffect( () => {
+  useEffect(() => {
     console.log('in useEffect');
-    
+
     // initiate GET request on page load
     getGalleryItems();
   }, [])
+  // =============== END USE EFFECT ===================
+
+
+  // =============== GET GALLERY ITEMS ================
+  const getGalleryItems = () => {
+    console.log('in getGalleryItems');
+
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log('Response from GET:', response);
+
+      // reassigns state of galleryItems from empty array to array of objects held in server
+      setGalleryItems(response.data);
+    }).catch((error) => {
+      console.log('Error from GET:', error);
+    })
+  }
+  // ============= END GET GALLERY ITEMS ===============
+
+  console.log('Gallery items array:', galleryItems);
 
   return (
     <div className="App">
