24d4f75d55b82ce4edf2a6e783dee1fe7ff050b5
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-19 12:21:35 -0600

    Added objects in galleryItems array, which included adding a description for each image

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,17 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 0, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/1-Viral.png', description: `The Bernie phenomenon was the peak of my marketing career. It remains the most liked image on this company's page.`, likes: 0 },
+    { id: 2, path: 'images/2-Peaceful-Desolation.png', description: 'This may or may not have taken the entire day to build. Totally worth it.', likes: 0 },
+    { id: 3, path: 'images/3-Gotcha.png', description: `My life changed for the better ten years ago, when we adopted my sisters from Ethiopia.`, likes: 0 },
+    { id: 4, path: 'images/4-Weekend-Getaway.png', description: 'Weekend getaways are good for the sole. Destination: Red Wing, MN', likes: 0 },
+    { id: 5, path: 'images/5-Overtime.png', description: 'Extra hours and odd jobs were a big part of 2021. This weekend project was a long one.', likes: 0 },
+    { id: 6, path: 'images/6-Views-on-Views.png', description: 'Every once in a while you get a view you have to capture. This one was one of them.', likes: 0 },
+    { id: 7, path: 'images/7-Freedom.png', description: `My wife and I paid off $25.909.14 of debt in seven months. Many sacrifices were made, but the reward will last forever.`, likes: 0 },
+    { id: 8, path: 'images/8-Meal-of-the-Year.png', description: 'Lemon pepper chicken over rice. Words cannot describe how heavenly this meal tasted.', likes: 0 },
+    { id: 9, path: 'images/9-Surfs-Up.png', description: 'Surfing in September? Absolutely.', likes: 0 },
+    { id: 10, path: 'images/10-Courage.png', description: `I spy myself at the bottom of this rock, a moment where courage conquered fear.`, likes: 0 },
+    { id: 11, path: 'images/11-First-Year.png', description: 'One year of marriage in the books! What better way to spend it than under a shark?', likes: 0 },
+    { id: 12, path: 'images/12-The-Beginning.png', description: `This project marks the start of a new chapter for me in becoming a software engineer. Look how far I've come!`, likes: 0 }
 ];
 
 module.exports = galleryItems;
